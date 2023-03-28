cffd08bf58b940c189524db038f0300a69cc3b95
Marc Lichtman <marclichtman@microsoft.com>
2023-03-28 05:20:46 -0400

    more cleanups to drx.py

--- DIFI_Validator/drx.py
+++ DIFI_Validator/drx.py
@@ -79,144 +79,128 @@ if os.getenv("DIFI_RX_MODE"):
 #PCAP_FILE = "difi-compliant.pcap"
 
 
-############################
-# primary function that decodes DIFI packets
-############################
+####################
+# Decode DIFI packet
+####################
 def decode_difi_vrt_packet(stream: BytesIO) -> Union[DifiStandardContextPacket,DifiVersionContextPacket,DifiDataPacket]:
     """
     Read DIFI VRT packet from byte stream, decode it and return class object filled with its data.
-
-    :param stream: raw data stream of bytes to decode
     """
 
-    #get packet type
+    # packet type
     tmpbuf = stream.read1(4)
     if not tmpbuf:
         return None
     (value,) = struct.unpack(">I", tmpbuf)
     packet_type = (value >> 28) & 0x0f   #(bit 28-31)
 
-    #get stream id
+    # stream id
     tmpbuf = stream.read1(4)
     (value,) = struct.unpack(">I", tmpbuf)
     stream_id = value
 
     stream.seek(0) #reset stream back to beginning
 
-    #create instance of packet class for packet type (standard context, version context or data packet)
+    # create instance of packet class for packet type and parse packet
     if packet_type == DIFI_STANDARD_FLOW_SIGNAL_CONTEXT:
         return DifiStandardContextPacket(stream)
     elif packet_type == DIFI_VERSION_FLOW_SIGNAL_CONTEXT:
         return DifiVersionContextPacket(stream)
     elif packet_type == DIFI_STANDARD_FLOW_SIGNAL_DATA_WITH_STREAMID:
         return DifiDataPacket(stream)
-
-    #not valid data packet because no stream id
-    elif packet_type == DIFI_STANDARD_FLOW_SIGNAL_DATA_NO_STREAMID:
+    elif packet_type == DIFI_STANDARD_FLOW_SIGNAL_DATA_NO_STREAMID: # DIFI doesnt support this type of packet
         raise NoncompliantDifiPacket("non-compliant DIFI data packet type [data packet without stream ID packet type: 0x%1x]  (must be [0x%1x] standard context packet, [0x%1x] version context packet, or [0x%1x] data packet)" % (packet_type, DIFI_STANDARD_FLOW_SIGNAL_CONTEXT, DIFI_VERSION_FLOW_SIGNAL_CONTEXT, DIFI_STANDARD_FLOW_SIGNAL_DATA_WITH_STREAMID), DifiInfo(packet_type=packet_type, stream_id=stream_id))
-    #not valid DIFI packet
     else:
         raise NoncompliantDifiPacket("non-compliant DIFI packet type [0x%1x]  (must be [0x%1x] standard context packet, [0x%1x] version context packet, or [0x%1x] data packet)" % (packet_type, DIFI_STANDARD_FLOW_SIGNAL_CONTEXT, DIFI_VERSION_FLOW_SIGNAL_CONTEXT, DIFI_STANDARD_FLOW_SIGNAL_DATA_WITH_STREAMID), DifiInfo(packet_type, stream_id=stream_id))
 
 
 ###############
-# misc debugging-function to output roughly estimated packets p/sec to console
+# Recursive function to output estimated packets p/sec to console
 ##############
 def estimate_pkts_per_sec(counts: list)->int:
-
-    #curr count - prev count
-    per_sec_count = counts[0][1] - counts[0][0]
+    per_sec_count = counts[0][1] - counts[0][0] # curr count - prev count
     print("pkts p/sec: {}".format(per_sec_count))
     timer = threading.Timer(1.0, estimate_pkts_per_sec, [counts])
-    #change prev count to curr count
-    counts[0][0] = counts[0][1]
+    counts[0][0] = counts[0][1] # change prev count to curr count
     timer.daemon = True #allows timer to stop when main thread exits
     timer.start()
 
 
 ################
-# function for processing data received from DIFI device
+# Processing packet received
 ################
 def process_data(data: Union[bytes,BytesIO]):
+    if data is None:
+        print("packet received, but data empty.")
+        return
+    try:
+        if type(data) is bytes:
+            stream = io.BytesIO(data)
+        else:
+            stream = data
 
-    if data is not None:
+        pkt = decode_difi_vrt_packet(stream) # parse data to create packet object
+        #print("type: %s" % (type(pkt).__name__))
 
-        try:
-            if type(data) is bytes:
-                stream = io.BytesIO(data)
-            else:
-                stream = data
-
-            #parse data to create packet object
-            pkt = decode_difi_vrt_packet(stream)
-            #print("type: %s" % (type(pkt).__name__))
-
-            #show resulting packet object in console
-            if type(pkt) is DifiStandardContextPacket:
-                if VERBOSE or DEBUG: print("\
+        # show resulting packet object in console
+        if type(pkt) is DifiStandardContextPacket:
+            if VERBOSE or DEBUG: print("\
 -----------------------------\r\n\
 -- standard context packet --\r\n\
 -----------------------------\r\n\
 %s\r\n\r\n%s" % (pkt.to_json(JSON_AS_HEX), str(pkt)))
 
-                #update 'compliant' archive files
-                write_compliant_count_to_file(pkt.stream_id)
-                if SAVE_LAST_GOOD_PACKET:
-                    write_compliant_to_file(pkt)
+            write_compliant_count_to_file(pkt.stream_id) # update 'compliant' archive files
+            if SAVE_LAST_GOOD_PACKET:
+                write_compliant_to_file(pkt)
 
-            elif type(pkt) is DifiVersionContextPacket:
-                if VERBOSE or DEBUG: print("\
+        elif type(pkt) is DifiVersionContextPacket:
+            if VERBOSE or DEBUG: print("\
 ----------------------------\r\n\
 -- version context packet --\r\n\
 ----------------------------\r\n\
 %s\r\n\r\n%s" % (pkt.to_json(JSON_AS_HEX), str(pkt)))
 
-                #update 'compliant' archive files
-                write_compliant_count_to_file(pkt.stream_id)
-                if SAVE_LAST_GOOD_PACKET:
-                    write_compliant_to_file(pkt)
+            write_compliant_count_to_file(pkt.stream_id) # update 'compliant' archive files
+            if SAVE_LAST_GOOD_PACKET:
+                write_compliant_to_file(pkt)
 
-            elif type(pkt) is DifiDataPacket:
-                if VERBOSE or DEBUG: print("\
+        elif type(pkt) is DifiDataPacket:
+            if VERBOSE or DEBUG: print("\
 -----------------\r\n\
 -- data packet --\r\n\
 -----------------\r\n\
 %s\r\n\r\n%s" % (pkt.to_json(JSON_AS_HEX), str(pkt)))
 
-                #update 'compliant' archive files
-                write_compliant_count_to_file(pkt.stream_id)
-                if SAVE_LAST_GOOD_PACKET:
-                    write_compliant_to_file(pkt)
-            else:
-                #ignore if not standard context, version context or data packet
-                pass
+            write_compliant_count_to_file(pkt.stream_id) # update 'compliant' archive files
+            if SAVE_LAST_GOOD_PACKET:
+                write_compliant_to_file(pkt)
+        else:
+            #ignore if not standard context, version context or data packet
+            pass
 
-        except NoncompliantDifiPacket as e:
-            #update 'non-compliant' archive files
-            write_noncompliant_to_file(e)
-            if VERBOSE or DEBUG: print(e.message)
-            if VERBOSE or DEBUG: print("--> not DIFI compliant, packet not decoded:\r\n%s" % e.difi_info.to_json())
-            write_noncompliant_count_to_file(e.difi_info.stream_id)
-        except InvalidDataReceived as e:
-            print("---------------")
-            pprint.pprint(e)
-            print("---------------")
-        except InvalidPacketType as e:
-            print("---------------")
-            pprint.pprint(e)
-            print("---------------")
-        except Exception as e:
-            print("---------------")
-            print("packet decode error --> {}".format(e))
-            print("---------------")
-    else:
-        print("packet received, but data empty.")
+    except NoncompliantDifiPacket as e:
+        write_noncompliant_to_file(e) # update 'non-compliant' archive files
+        if VERBOSE or DEBUG: print(e.message)
+        if VERBOSE or DEBUG: print("--> not DIFI compliant, packet not decoded:\r\n%s" % e.difi_info.to_json())
+        write_noncompliant_count_to_file(e.difi_info.stream_id)
+    except InvalidDataReceived as e:
+        print("---------------")
+        pprint.pprint(e)
+        print("---------------")
+    except InvalidPacketType as e:
+        print("---------------")
+        pprint.pprint(e)
+        print("---------------")
+    except Exception as e:
+        print("---------------")
+        print("packet decode error --> {}".format(e))
+        print("---------------")
 
 
-
-###################
-# class to implement asyncio UDP server mode
-###################
+#########################
+# Asyncio UDP server mode
+#########################
 class DifiProtocol(asyncio.DatagramProtocol):
     def __init__(self, on_connection_lost: asyncio.Future):
         super().__init__()
@@ -233,11 +217,9 @@ class DifiProtocol(asyncio.DatagramProtocol):
         self.on_connection_lost.set_result=True
 
     def datagram_received(self, data, addr):
-
         self.count+=1
         print('received {} bytes from {}. [count={}]'.format(len(data), addr, self.count))
         #print("packet data received: {}".format(data))
-
         #data = data.decode()
 
         #may want to put in queue instead and process data separately
@@ -245,37 +227,30 @@ class DifiProtocol(asyncio.DatagramProtocol):
         #print("queue size: ", self.q.qsize())
 
         process_data(data)
-
         print('waiting to receive packet data...')
 
+
 ################
-# main loop for asyncio UDP server mode
+# Main loop for asyncio UDP server mode
 ################
 async def asyncio_main_loop():
-
     print('starting asyncio UDP listener on {} port {}...'.format(DIFI_RECEIVER_ADDRESS, DIFI_RECEIVER_PORT))
-
-    #event loop
-    loop = asyncio.get_running_loop()
-
+    loop = asyncio.get_running_loop() # event loop
     on_connection_lost = loop.create_future()
 
     #creates one protocol instance to serve all clients
-    transport, protocol = await loop.create_datagram_endpoint(  # pylint: disable=unused-variable
+    transport, _ = await loop.create_datagram_endpoint(
         lambda: DifiProtocol(on_connection_lost),
         local_addr=(DIFI_RECEIVER_ADDRESS, DIFI_RECEIVER_PORT))
-
     try:
         await on_connection_lost
     finally:
         transport.close()
 
 
-
 ######################################
-# main script
+# Main/Entrypoint
 ######################################
-
 def main():
     # pylint: disable=global-statement,global-variable-not-assigned
 
@@ -351,25 +326,18 @@ def main():
     #print("show pkts p/sec: ", SHOW_PKTS_PER_SEC)
     #sys.exit(0)
 
-
-
-    #truncate all difi output files on startup
-    truncate_all_difi_files()
-    
+    truncate_all_difi_files() # truncate all difi output files on startup
     #delete all difi output files on startup
     #delete_all_difi_files()
 
-
     ##########
-    # asyncio udp socket server mode, listening for packets to decode
+    # Asyncio udp socket server mode, listening for packets to decode
     ##########
     if MODE == MODE_ASYNCIO:
-
         #run async server
         print("entering asyncio.run...")
         asyncio.run(asyncio_main_loop())
         print("exited asyncio.run.")
-
         #print("entering asyncio.run...")
         #loop = asyncio.get_event_loop()
         #t = loop.create_datagram_endpoint(DifiProtocol, local_addr=(DIFI_RECEIVER_ADDRESS, DIFI_RECEIVER_PORT))
@@ -377,18 +345,12 @@ def main():
         #loop.run_forever()
         #print("exited asyncio.run.")
 
-
-
     ##########
-    # udp socket server mode, listening for packets to decode
+    # UDP socket server mode, listening for packets to decode
     ##########
     elif MODE == MODE_SOCKET:
-
-        #create UDP socket listener
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-        #bind to port
-        server_address = (DIFI_RECEIVER_ADDRESS, DIFI_RECEIVER_PORT)
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # create UDP socket listener
+        server_address = (DIFI_RECEIVER_ADDRESS, DIFI_RECEIVER_PORT) # bind to port
         print('starting UDP listener on {} port {}...'.format(*server_address))
         sock.bind(server_address)
 
@@ -397,56 +359,51 @@ def main():
             prev_curr_count = [0,0]
             estimate_pkts_per_sec([prev_curr_count])
 
+        # listen for packets
         recv_count = 0
-
-        #listen for packets
         while True:
-
             print('waiting to receive packet data...')
             data, address = sock.recvfrom(9216) #max data packet 9000 bytes, per difi spec
-
-            recv_count+=1
+            recv_count += 1
             if SHOW_PKTS_PER_SEC: prev_curr_count[1] = recv_count
             print('received {} bytes from {}. [count={}]'.format(len(data), address, recv_count))
             #print("packet data received: {}".format(data))
-
             process_data(data)
 
 
+    #########
+    # PCAP file mode
+    #########
+    ''' CURRENTLY DISABLED
+    elif MODE == MODE_PCAP:
+        from pypacker import ppcap
+        from pypacker.pypacker import byte2hex
+        from pypacker.layer12 import ethernet
+        from pypacker.layer3 import ip
+        from pypacker.layer4 import tcp
+        from pypacker.layer4 import udp
+        preader = ppcap.Reader(filename=PCAP_FILE)
 
-    #pcap mode is currently disabled
-    ##########
-    #decode pcap file mode
-    ##########
-    #elif MODE == MODE_PCAP:
-    #from pypacker import ppcap
-    #from pypacker.pypacker import byte2hex
-    #from pypacker.layer12 import ethernet
-    #from pypacker.layer3 import ip
-    #from pypacker.layer4 import tcp
-    #from pypacker.layer4 import udp
-    #    preader = ppcap.Reader(filename=PCAP_FILE)
-    #
-    #    for ts, buf in preader:
-    #
-    #        eth = ethernet.Ethernet(buf)
-    #
-    #        if eth[ethernet.Ethernet,ip.IP] is not None:
-    #
-    #            #print("packet src\dest:  %s -> %s  [ts=%d]" % (eth[ip.IP].src_s, eth[ip.IP].dst_s, ts))
-    #            #print("full packet: %s" % (eth.bin().hex()))
-    #
-    #            b = eth.bin()
-    #            stream = io.BytesIO(b)
-    #            eth_ip_udp_header = stream.read1(42)  #jump past: eth frame/ip hdr/udp hdr (42 bytes)
-    #            try:
-    #                #print("proto: ", eth_ip_udp_header[23:24])
-    #                #only UDP packets
-    #                if eth_ip_udp_header[23:24] == UDP_PROTO:
-    #                    process_data(stream)
-    #            except Exception as e:
-    #                pprint.pprint(e)
+        for ts, buf in preader:
 
+            eth = ethernet.Ethernet(buf)
+
+            if eth[ethernet.Ethernet,ip.IP] is not None:
+
+                #print("packet src\dest:  %s -> %s  [ts=%d]" % (eth[ip.IP].src_s, eth[ip.IP].dst_s, ts))
+                #print("full packet: %s" % (eth.bin().hex()))
+
+                b = eth.bin()
+                stream = io.BytesIO(b)
+                eth_ip_udp_header = stream.read1(42)  #jump past: eth frame/ip hdr/udp hdr (42 bytes)
+                try:
+                    #print("proto: ", eth_ip_udp_header[23:24])
+                    #only UDP packets
+                    if eth_ip_udp_header[23:24] == UDP_PROTO:
+                        process_data(stream)
+                except Exception as e:
+                    pprint.pprint(e)
+    '''
 
 if __name__ == '__main__':
     main()
