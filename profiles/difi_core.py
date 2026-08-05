2781ed53e5c541f3a6cfb2e1ad12266b7d10cad7
Marc Lichtman <marcll@vt.edu>
2026-08-05 14:14:07 -0400

    add the core and high rate profile

--- profiles/difi_core.py
+++ profiles/difi_core.py
@@ -1,5 +1,3 @@
-# This is not an official DIFI profile, it's just used to test the software, as it is based on the Example1 pcap
-
 from packet_definitions.construct_custom_types import *
 
 # Profile-specific Validations (keep DIFI version agnostic wherever possible)
@@ -7,7 +5,9 @@ from packet_definitions.construct_custom_types import *
 # Put any header-specific checks in the context category
 def validate_profile_context(profile, parsed):
     errors = []
-    if parsed.sampleRate != 1e6: errors.append("Sample rate doesnt match profile")
+    if parsed.sampleRate != 20e6: errors.append("Sample rate doesnt match profile")
+    if parsed.dataPacketFormat.data_item_size != 7: errors.append("Was not 8-bit data_item_size")
+    if parsed.header.pktSize != 360: errors.append("Expected pktSize=360")
     return errors
 
 def validate_profile_data(profile, parsed):
